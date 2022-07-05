d9a36ea7e0bd1c7772308b0814999f6393636137
javenell <jamesave3@gmail.com>
2022-07-06 10:44:44 +1200

    added commenting

--- python/render.py
+++ python/render.py
@@ -54,7 +54,7 @@ def convert_coords(x, y):
     # math to convert coords
     x_prime = 960 + cos(30 * pi / 180) * x - cos(30 * pi / 180) * y
     y_prime = sin(30 * pi / 180) * x + sin(30 * pi / 180) * y
-    return int(x_prime), int(y_prime)
+    return int(x_prime), int(y_prime)  # return converted coordinates
 
 
 def clamp(n, minn, maxn):
@@ -80,7 +80,7 @@ class Round:
 
     class Turn:
         """
-        This class represents a Round Turn.
+        This class represents a Turn.
         """
 
         list = []  # Turn list
@@ -91,7 +91,7 @@ class Round:
 
     def get_turn(self, number):
         """
-        This function gets a specified turn from a supplied number
+        This function gets a specified turn from a supplied turn number
         :param number: Wanted turn number
         :type number: int
         :return: specified Turn object
@@ -193,7 +193,7 @@ def main():
     # get info about the game form the kekule games API
     game_info = requests.request('GET', f"{API_BASE_URL}/{game_id}/", headers={'Authorization': API_AUTH}).json()
 
-    if online: # if online mode enabled
+    if online:  # if online mode enabled
         s3 = boto3.client("s3")  # set up s3 connection
         # download gamelog from S3 bucket and save
         s3.download_file(
@@ -206,55 +206,51 @@ def main():
         content = content.partition(f'game = {round_id}')  # partition to get the start of game specified
         content = content[2].partition(f'game = {int(round_id) + 1}')  # get lines to the end of the game
         content = content[0].split('\n')  # split into list by new line
+        # remove unwanted spacing and tagging if it exists
         if content[0] == '':
             content.pop(0)
         if content[-1] == '':
             content.pop(-1)
         elif content[-1] == "end":
             content.pop(-1)
-    data = Round()
+    data = Round()  # create round
 
-    error_count = 0
-    for turn in content:
+    for turn in content:  # for turn in input file
         turn = eval(turn)
-        for tup in turn:
-            if tup[0] not in range(0, 100) or tup[1] not in range(0, 100):
-                error_count += 1
-                if error_count <= 3:
-                    warnings.warn(f"Token outside board! Ignoring Turn. {error_count} of 3", RuntimeWarning)
-                else:
-                    raise RuntimeError(
-                        f"Token outside board! To many stray Tokens. Please check the log file {game_id}.gamelog, round {round_id}. Terminating")
+        for tup in turn:  # for tuple in turn list
+            if tup[0] not in range(0, 100) or tup[1] not in range(0, 100):  # if token not on board
+                raise RuntimeError(
+                    f"Token outside board! To many stray Tokens. Please check the log file {game_id}.gamelog, round {round_id}. Terminating")
         data.add_turn(turn)
 
-    rstr_all = []
-    bstr_all = []
+    rstr_all = []  # persistent across turns red strength data
+    bstr_all = []  # persistent across turns blue strength data
 
     for turn_number in range(1, data.get_number_turns()):
-        rstr = 0
-        bstr = 0
-        hist = [[], []]
-        all_draw = []
-        turn = data.get_turn(turn_number)
+        rstr = 0  # red strength
+        bstr = 0  # blue strength
+        hist = [[], []]  # data for histogram
+        all_draw = []   # tokens to draw
+        turn = data.get_turn(turn_number)  # retrieve turn
 
-        img = Image.new("RGB", (1920, 1080))
-        img1 = ImageDraw.Draw(img)
+        img = Image.new("RGB", (1920, 1080))  # create PIL image
+        img1 = ImageDraw.Draw(img)  # set up drawing on PIL image
         if dark:
-            img1.rectangle((0, 0) + img.size, fill='black')
-            white = "black"
-            black = "white"
+            img1.rectangle((0, 0) + img.size, fill='black')  # draw background
+            white = "black"  # set colour of everything that would normally be white to black
+            black = "white"  # set colour of everything that would normally be balck to white
         else:
-            img1.rectangle((0, 0) + img.size, fill='white')
-            line_fill = "#b4b4b4"
-            white = "white"
-            black = "black"
+            img1.rectangle((0, 0) + img.size, fill='white')  # draw background
+            white = "white"  # set white to be white since dark mode is not enabled
+            black = "black"  # set black to be black since dark mode is not enabled
 
         if dark:
             plt.style.use('dark_background')
         img1.polygon(
             [(convert_coords(0, 0)), (convert_coords(0, 1010)), (convert_coords(1010, 1010)), (convert_coords(1010, 0))],
-            white, black)
+            white, black)  # make board outline
 
+        """ The following creates squares within the previously made board"""
         # vertical lines at an interval of "line_distance" pixel
         for x in range(10, 1010, 10):
             img1.line(convert_coords(x, 0) + convert_coords(x, 1010), fill="#b4b4b4")
@@ -262,6 +258,7 @@ def main():
         for y in range(10, 1010, 10):
             img1.line(convert_coords(0, y) + convert_coords(1010, y), fill="#b4b4b4")
 
+        # draw grey box below board
         img1.polygon(
             [(convert_coords(0, 1010)),
              (85, 523),
@@ -270,6 +267,7 @@ def main():
             fill='grey'
         )
 
+        # draw grey box below board
         img1.polygon(
             [(1835, 505),
              (1835, 525),
@@ -278,25 +276,34 @@ def main():
             fill='grey'
         )
 
-        for tup in turn.list:
-            if tup[3] == 'B':
+        for tup in turn.list:  # for token in turn
+            if tup[3] == 'B':  # if the token is blue's
+                # change blue colour according to the tokens strength
                 rgb = (clamp(220 - (11 * int(tup[2])), 0, 255), clamp(220 - (11 * int(tup[2])), 0, 255), 255)
+                # convert rgb values to hex
                 hex_result = "".join([format(val, '02X') for val in rgb])
                 outline_cl = 'blue'
+                # add the strength of this token to the blue total
                 bstr += tup[2]
+                # add the strength of this token to the blue histogram dat
                 hist[0].append(tup[2])
             elif tup[3] == 'R':
+                # change red colour according to the tokens strength
                 rgb = (255, clamp(220 - (11 * int(tup[2])), 0, 255), clamp(220 - (11 * int(tup[2])), 0, 255))
+                # convert rgb values to hex
                 hex_result = "".join([format(val, '02X') for val in rgb])
                 outline_cl = 'red'
+                # add the strength of this token to the red total
                 rstr += tup[2]
+                # add the strength of this token to the red histogram dat
                 hist[1].append(tup[2])
 
-
+            # generate coordinates of the bottom of the token to be drawn later on
             bottom_coords = [(convert_coords(tup[0] * 10, (tup[1] * 10) + 10)),
                              (convert_coords((tup[0] * 10) + 10, (tup[1] * 10) + 10)),
                              (convert_coords((tup[0] * 10) + 10, tup[1] * 10))]
 
+            # generate intermediary coordinates of the top of the token
             in_coords = [(convert_coords(tup[0] * 10 + 2, tup[1] * 10 + 2)),
                          (convert_coords(tup[0] * 10 + 2, (tup[1] * 10) + 10 - 2)),
                          (convert_coords((tup[0] * 10) + 10 - 2, (tup[1] * 10) + 10 - 2)),
@@ -305,13 +312,17 @@ def main():
             top_coords = []
 
             for tup in in_coords:
-                tup_out = (tup[0], tup[1] - 5)
+                tup_out = (tup[0], tup[1] - 5)  # remove 5 pixels from the intermediate coordinates to make them go
+                # up the image when drawn
                 top_coords.append(tup_out)
 
+            """ The following creates the coordinates needed for 
+            drawing the fill between the top and bottom of the token """
             fill_coords = bottom_coords.copy()
             int_coords = top_coords.copy()[1:]
             int_coords.sort(reverse=True)
             fill_coords.extend(int_coords)
+            # add all the coordinates and colour to a dict then append to the list to be drawn later
             all_draw.append({"fill": fill_coords,
                              "bottom": bottom_coords,
                              "top": top_coords,
@@ -319,18 +330,21 @@ def main():
                              "outline": outline_cl
                              })
 
+        # draw the bottom of all the tokens
         for token in all_draw:
             img1.line(
                 token["bottom"],
                 fill=token["outline"],
             )
 
+        # draw the fill of all the tokens
         for token in all_draw:
             img1.polygon(
                 token["fill"],
                 fill=f'#{token["hex"]}'
             )
 
+        # draw the top of all the tokens
         for token in all_draw:
             img1.polygon(
                 token["top"],
@@ -338,67 +352,73 @@ def main():
                 outline=token["outline"]
             )
 
+        # append strengths to all strength list for each player
         rstr_all.append(rstr)
         bstr_all.append(bstr)
-        x = [i for i in range(len(bstr_all))]
 
         if dark:
             plt.style.use('dark_background')
+
+        x = [i for i in range(len(bstr_all))]  # make x coord for plot
+        # plot lines
         plt.plot(x, rstr_all, color='red')
         plt.plot(x, bstr_all, color='blue')
+        # add labels
         plt.xlabel("Turn Number")
         plt.ylabel("Total Strength")
 
-        img_buf = io.BytesIO()
+        img_buf = io.BytesIO()  # set up buffer to save plot to
 
-        plt.savefig(img_buf, format='png', bbox_inches='tight', pad_inches=0)
+        plt.savefig(img_buf, format='png', bbox_inches='tight', pad_inches=0)  # save plot
         plt.close()
-        plot = Image.open(img_buf)
-        plot.thumbnail((400, 400), Image.ANTIALIAS)
-        img.paste(plot, (40, 2))
+        plot = Image.open(img_buf)  # open plot in PIL
+        plot.thumbnail((400, 400), Image.ANTIALIAS)  # resize plot
+        img.paste(plot, (40, 2))  # paste onto main image
 
         img_buf.close()
-        img_buf = io.BytesIO()
+        img_buf = io.BytesIO()  # set up buffer to save plot to
 
         if dark:
             plt.style.use('dark_background')
-        plt.hist(hist, histtype='bar', label=['blue', 'red'], color=['blue', 'red'], log=True)
+        plt.hist(hist, histtype='bar', label=['blue', 'red'], color=['blue', 'red'], log=True)  # make plot
+        # add labels
         plt.ylabel("Number")
         plt.xlabel("Strength")
-        plt.legend()
-        plt.savefig(img_buf, format='png', bbox_inches='tight', pad_inches=0)
+        plt.legend()  # add legend
+
+        plt.savefig(img_buf, format='png', bbox_inches='tight', pad_inches=0)  # save plot
         plt.close()
-        plot = Image.open(img_buf)
-        plot.thumbnail((400, 400), Image.ANTIALIAS)
-        img.paste(plot, (40, 750))
+        plot = Image.open(img_buf)  # open plot in PIL
+        plot.thumbnail((400, 400), Image.ANTIALIAS)  # resize plot
+        img.paste(plot, (40, 750))  # paste onto main image
 
         Font20 = ImageFont.truetype('DejaVuSans.ttf', 20)
-        Font10 = ImageFont.truetype('DejaVuSans.ttf', 10)
 
-        # Add Text to an image
+        # Add number of tokens to image
         img1.text((1650, 785), "# Tokens", font=Font20, fill=black)
         img1.text((1650, 815), f"Red {len(hist[1])}", font=Font20, fill="red")
         img1.text((1650, 845), f"Blue {len(hist[0])}", font=Font20, fill="blue")
 
+        # add player legend to image
         img1.rectangle([(1650, 70), (1690, 90)], fill="blue")
         img1.text((1710, 70), game_info['player1'], font=Font20, fill=black)
         img1.rectangle([(1650, 110), (1690, 130)], fill="red")
         img1.text((1710, 110), game_info['player2'], font=Font20, fill=black)
 
-        img.save(f"{turn_number}.png")
+        img.save(f"{turn_number}.png")  # save main image to disk
 
-    convert_frames_to_video(os.getcwd(), f'{game_id}_{round_id}.mp4', 24 * (speed / 100))
+    convert_frames_to_video(os.getcwd(), f'{game_id}_{round_id}.mp4', 24 * (speed / 100))  # convert all images to video
 
-    if online:
+    if online:  # if online mode enabled
 
+        # Upload resultant mp4 video to S3
         s3.upload_file(f'{game_id}_{round_id}.mp4', "kekule-web-media", f'video/{game_id}_{round_id}.mp4',)
-
 
         payload = {'rendered': True}
         headers = {'Authorization': API_AUTH}
         url = f"https://kekule.games/GL/API/Gamelist/{game_id}/"
 
-        response = requests.request("PUT", url, headers=headers, data=payload)
+        response = requests.request("PUT", url, headers=headers, data=payload)  # Update Kekule Games' system via API
         if response.status_code != 200:
             warnings.warn("Possible failure communicating to Kekule Games API. Please check server log", Warning)
 
@@ -406,4 +426,4 @@ def main():
 if __name__ == "__main__":
     # Get Kekule Games API credentials from the environment
     API_AUTH = os.environ['API_KEY']
-    main()
+    main()  # run main code
--- python/test_render.py
+++ python/test_render.py
@@ -7,9 +7,9 @@ class TestConvertCoords(unittest.TestCase):
         """
         Test that the convert_coords function is mathematically correct
         """
-        data = [[(0, 0), (960, 0)], [(0, 1000), (93, 499)], [(1000, 1000), (960, 999)], [(1000, 0), (1826, 499)]]  #
         # Test data and expected results in nested list
-        for test_data in data:
+        data = [[(0, 0), (960, 0)], [(0, 1000), (93, 499)], [(1000, 1000), (960, 999)], [(1000, 0), (1826, 499)]]
+        for test_data in data:  # run several tests
             result = render.convert_coords(test_data[0][0], test_data[0][1])
             self.assertEqual(result, test_data[1])
 
